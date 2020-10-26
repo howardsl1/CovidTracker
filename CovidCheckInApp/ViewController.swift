f7ec7059b132fd7a0e980d3a244e94c2fae4d132
Sam Howard <howardsl1@appstate.edu>
2020-10-26 13:07:33 -0400

    Initial Commit

---
+++ CovidCheckInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidCheckInApp
+//
+//  Created by Sam Howard on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
